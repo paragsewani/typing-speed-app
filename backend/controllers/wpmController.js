bff1d78a663e472196e4c4f8c90e6840bb616626
paragsewani <142333190+paragsewani@users.noreply.github.com>
2025-07-19 00:58:14 +0530

    Update wpmController.js

--- backend/controllers/wpmController.js
+++ backend/controllers/wpmController.js
@@ -4,17 +4,17 @@ exports.saveWPM = async (req, res) => {
   const { wpm } = req.body;
   const userId = req.userId;
 
-  // Save new WPM record
+  // It saves new WPM record
   await WPMRecord.create({ userId, wpm });
 
-  // Count how many WPM records the user has
+  // it Counts how many WPM records the user has
   const count = await WPMRecord.countDocuments({ userId });
 
-  // If more than 5, delete the oldest ones
+  // If more than 5, we will delete the first record
   if (count > 5) {
     const oldest = await WPMRecord.find({ userId })
-      .sort({ timestamp: 1 }) // oldest first
-      .limit(count - 5); // get the extra ones
+      .sort({ timestamp: 1 }) 
+      .limit(count - 5); 
     const idsToDelete = oldest.map((r) => r._id);
     await WPMRecord.deleteMany({ _id: { $in: idsToDelete } });
   }
